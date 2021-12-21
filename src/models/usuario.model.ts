e4e24e885b4a361303313c921c95e77d3282d734
Roberto <roberto.rousse@gmail.com>
2021-12-21 14:03:37 -0300

    updating usuarios

--- src/models/usuario.model.ts
+++ src/models/usuario.model.ts
@@ -1,4 +1,32 @@
 import mongoose from 'mongoose';
 import UsuarioSchema from '../schemas/usuario.schema';
+import {
+  IUsuario,
+  IUsuarioNuevo,
+  BaseClassUsuario,
+} from '../interfaces/usuario.interface';
 
-const UsuarioModel = mongoose.model('Usuario', UsuarioSchema);
+// const UsuarioModel = mongoose.model('Usuario', UsuarioSchema);
+
+class UsuarioModel implements BaseClassUsuario {
+  private usuarios;
+  constructor() {
+    this.usuarios = mongoose.model<IUsuario>('Usuarios', UsuarioSchema);
+  }
+  async add(data: IUsuarioNuevo): Promise<IUsuario> {
+    const nuevo = new this.usuarios(data);
+    nuevo.save();
+    return nuevo;
+  }
+
+  async update(
+    id: string,
+    dataUsuario: IUsuarioNuevo
+  ): Promise<IUsuario | null> {
+    return this.usuarios.findByIdAndUpdate(id, dataUsuario);
+  }
+
+  async delete(id: string): Promise<void> {
+    this.usuarios.findByIdAndDelete(id);
+  }
+}
